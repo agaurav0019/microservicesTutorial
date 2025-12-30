13ddc36cf54a12c9899851396cc5ecc5a0d566a4
Abhishek Gaurav <gaurav.abhishek99@gmail.com>
2025-12-30 22:28:01 +0530

    Implemented Fetch Account Details and update account details controller

--- src/main/java/com/eazybytes/accounts/service/IAccountService.java
+++ src/main/java/com/eazybytes/accounts/service/IAccountService.java
@@ -8,4 +8,18 @@ public interface IAccountService {
      * @Param customerDto- CustomerDto Object
      */
     void createAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @Param Mobile Number
+     * @return Account details based on a given mobile number
+     */
+    CustomerDto fetchAccount(String mobileNumber);
+
+    /**
+     *
+     * @Param CustomerDto
+     * @return boolean indicating if the update of account details is successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
 }
